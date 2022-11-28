f7032a43d6bbbecf8c91d51ed26346f4400d0731
WalaaAtiah <walaaatiah97@gmail.com>
2022-11-28 20:20:13 +0000

    feat:creat first django project

---
+++ add_templets/urls.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from django.urls import path
+from .views import HomePage,AboutPage
+
+urlpatterns = [
+    path('', HomePage.as_view(),name="home"),
+    path('about', AboutPage.as_view(),name="about"),
+
+]
